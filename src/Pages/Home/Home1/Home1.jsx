da5a042fc0ffd28f9eb76aca2b403b0eb6bfc7ef
JahongirFox <abdurashidovabdulatif605@gmail.com>
2025-03-27 12:09:08 +0500

    Home Page is changed

--- src/Pages/Home/Home1/Home1.jsx
+++ src/Pages/Home/Home1/Home1.jsx
@@ -5,7 +5,7 @@ function Home1() {
             <div className="Home">
             
                 {/* <p className="Home-p">Закажите качественные моторные <br /> масла для своего производства</p> */}
-                <p className="Home-p">КАЧЕСТВЕННЫЕ СМАЗОЧНЫЕ <br /> МАТЕРИАЛЫ И МОТОРНЫЕ МАСЛА <br /> ОТ БРЕНДОВ: SHELL, MOBIL, <br /> TATNEFT/TANECO</p>
+                <p className="Home-p">ЗАКАЖИТЕ КАЧЕСТВЕННЫЕ <br /> СМАЗОЧНЫЕ МАТЕРИАЛЫ И МОТОРНЫЕ <br /> МАСЛА ОТ БРЕНДОВ: SHELL, MOBIL, <br /> TATNEFT/TANECO ДЛЯ СВОЕГО <br /> ПРОИЗВОДСТВА</p>
                 <div className="Home-Center">
                     <div className="flex-btn">
                         <button className="home-btn">Перейти к выбору</button>   
