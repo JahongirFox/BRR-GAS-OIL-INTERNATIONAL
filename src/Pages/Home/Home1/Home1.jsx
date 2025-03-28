5ccfef3b32f5e83e24c9d25e75cd988454e3c52d
JahongirFox <abdurashidovabdulatif605@gmail.com>
2025-03-28 14:59:40 +0500

    Responsive ficed

--- src/Pages/Home/Home1/Home1.jsx
+++ src/Pages/Home/Home1/Home1.jsx
@@ -8,7 +8,7 @@ function Home1() {
                 <p className="Home-p">ЗАКАЖИТЕ КАЧЕСТВЕННЫЕ <br /> СМАЗОЧНЫЕ МАТЕРИАЛЫ И МОТОРНЫЕ <br /> МАСЛА ОТ БРЕНДОВ: SHELL, MOBIL, <br /> TATNEFT/TANECO ДЛЯ СВОЕГО <br /> ПРОИЗВОДСТВА</p>
                 <div className="Home-Center">
                     <div className="flex-btn">
-                        <button className="home-btn">Перейти к выбору</button>   
+                        <button className="home-btn">Перейти к выбору</button>
                         <button className="home-btn2">Связаться с нами</button>
                     </div>
                 </div>
